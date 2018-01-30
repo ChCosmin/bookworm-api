5490a2e0223b2eb70025f7d0fa614bf94c921786
Cosmin Chinde <cosmin.chinde@gmail.com>
2018-01-30 15:00:01 +0200

    User signs up

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import Promise from 'bluebird';
+
 import auth from './routes/auth';
+import users from './routes/users';
 
 dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGODB_URL, { useMongoClient: true });
+
 app.use('/api/auth', auth);
+app.use('/api/users', users);
 
 app.post("/api/auth", (req, res) => {
   res.status(400).json({ errors: { global: "Invalid credentials" } });
