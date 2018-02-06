e90d9e2d6f887f8330c46053440b1a3569f6528c
Cosmin Chinde <cosmin.chinde@gmail.com>
2018-02-06 15:23:46 +0200

    reset password, confirm email

--- src/models/User.js
+++ src/models/User.js
@@ -13,29 +13,56 @@ const schema = new mongoose.Schema({
     unique: true
   },
   passwordHash: { type: String, required: true },
-  confirmed: { type: Boolean, default: false }
+  confirmed: { type: Boolean, default: false },
+  confirmationToken: { type: String, default: ""}
 }, { timestamps: true });
 
 schema.methods.isValidPassword = function isValidPassword(password) {
-  return  bcrypt.compareSync(password, this.passwordHash);
+  return bcrypt.compareSync(password, this.passwordHash);
 };
 
 schema.methods.setPassword = function setPassword(password) {
   this.passwordHash = bcrypt.hashSync(password, 10);
-}
+};
+
+schema.methods.setConfirmationToken = function setConfirmationToken() {
+  this.confirmationToken = this.generateJWT();
+};
+
+schema.methods.generateConfirmationUrl = function generateConfirmationUrl() {
+  return `${process.env.HOST}/confirmation/${this.confirmationToken}`;
+};
+
+schema.methods.generateResetPasswordLink = function generateResetPasswordLink() {
+  return `${process.env.HOST}/reset_password/${this.generateResetPasswordToken()}`;
+};
 
 schema.methods.generateJWT = function generateJWT() {
-  return jwt.sign({
-    email: this.email
-  }, process.env.JWT_SECRET);
-}
+  return jwt.sign(
+    {
+      email: this.email,
+      confirmed: this.confirmed
+    },
+    process.env.JWT_SECRET
+  );
+};
+
+schema.methods.generateResetPasswordToken = function generateResetPasswordToken() {
+  return jwt.sign(
+    {
+      _id: this._id
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }
+  );
+};
 
 schema.methods.toAuthJSON = function toAuthJSON() {
   return {
     email: this.email,
     confirmed: this.confirmed,
     token: this.generateJWT()
-  }
+  };
 };
 
 schema.plugin(uniqueValidator, { message: 'This email is already taken'});
